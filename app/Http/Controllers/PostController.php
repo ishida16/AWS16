25840178d1661d7851c06773183f2b5f54133776
ishida16 <hiro23@heelife.net>
2024-03-18 00:05:09 +0900

    解答17詳細表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,6 +11,12 @@ class PostController extends Controller
 	{
 	    //return $post->get();//$postの中身を戻り値にする。
 	    //return view('posts.index')->with(['posts' => $post->getByLimit()]);
-	    return view('posts.index')->with(['posts' => $post->getPaginateByLimit(1)]);
+	    return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
+	}
+	
+	public function show(Post $post)
+	{
+	    return view('posts.show')->with(['post' => $post]);
+	 //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
 	}
 }
